8a410504035d87072f992e02d7ea74778ecf8c5f
Guilherme Baptista <guisgb13@gmail.com>
2014-02-24 23:58:57 +0100

    Working on GCM support: XMPP client process

--- server.py
+++ server.py
@@ -1,16 +1,104 @@
 import os, sys, json, xmpp, random, string
 from flask import Flask, make_response
+from multiprocessing import Process, Manager
 
 server = Flask(__name__)
 server.config.from_object("config.DevelopmentConfig")
 
+# HTTP Endpoints
+# Communication to the server
+
 @server.route('/')
 def index():
+
 	response = make_response(json.dumps({'message':'ok'}), 200)
 	response.headers["Content-Type"] = "application/json"
 	return response
 
+
+# XMPP Client
+
+def random_id():
+	rid = ''
+	for x in range(8):
+		rid += random.choice(string.ascii_letters + string.digits)
+	return rid
+
+def start_xmpp_client(commn):
+
+	def callback_function_I(session, message):
+
+		gcm = message.getTags('gcm')
+
+		if gcm:
+			gcm_json = gcm[0].getData()
+			msg = json.loads(gcm_json)
+			if not msg_has_key('message_type'):
+				#can send response back here
+				send({'to': msg['from'],
+					'message_type': 'ack',
+					'message_id': msg['message_id']})
+				if msg.has_key('from'):
+					send_queue.
+					append({'to': msg['from'],
+						'message_id': random_id(),
+						'data': {'pong': 1}})
+#			elif ( msg['message_type'] == 'ack' 
+#			or msg['message_type'] == 'nack'):
+#				pass
+
+	def send(client, json_dict):
+
+		template = (
+		"""<message>
+			<gcm xmlns='google:mobile:data'>
+				{1}
+			</gcm>
+		</message>""")
+
+		client.send(xmpp.protocol.
+			Message(node=template.format(client.Bind.bound[0],
+				json.dumps(json_dict))))
+
+	def flush_queued_messages():
+
+		while len(send_queue):
+			send(send_queue.pop(0))
+
+
+	client = xmpp.Client('gcm.googleapis.com', debug=['socket'])
+	client.connect(server=(SERVER, PORT), secure=1, use_srv=False)
+	auth = client.auth(USERNAME, PASSWORD)
+	if not auth:
+		commn['status'] = -1
+		commn['description'] = 'xmpp client authentication failed'
+		sys.exit(1)
+
+	unacked_messages_quota = 1000
+	send_queue = []
+
+#	client.RegisterHandler('event1', callback_function)
+#	client.RegisterHandler('event2', callback_function_2)
+#	client.RegisterHandler('event3', callback_function_3)
+#	...
+	client.RegisterHandler('eventI', callback_function_I)
+#	...
+#	client.RegisterHandler('event', callback_function_N)
+
+
+	while True:
+		client.Process(1)
+		flush_queued_messages()
+
 if __name__ == "__main__":
 
+	commn = Manager().dict()
+	commn['status'] = 0
+	commn['description'] = 'about to start'
+
+	xmpp_client_process = Process(target=start_xmpp_client, args=(commn))
+	xmpp_client_process.daemon = True
+	xmpp_client_process.start()
+
 	port = int(os.environ.get("PORT", 5000))
-	server.run(host="0.0.0.0", port=port, debug=True)	
+	server.run(host="0.0.0.0", port=port)
