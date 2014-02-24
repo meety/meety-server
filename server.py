785863734b27843fd6b603052a930cec5d19dc97
Guilherme Santos <guisgb13@gmail.com>
2014-02-24 20:15:34 +0000

    Initial flask server files

---
+++ server.py
@@ -0,0 +1,15 @@
+import os, sys, json, xmpp, random, string
+from flask import Flask, make_response
+
+server = Flask(__name__)
+
+@server.route('/')
+def index():
+	response = make_response(json.dumps({'message':'ok'}), 200)
+	response.headers["Content-Type"] = "application/json"
+	return response
+
+if __name__ == "__main__":
+
+	port = int(os.environ.get("PORT", 5000))
+	server.run(host="0.0.0.0", port=port, debug=True)	
