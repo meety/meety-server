7a7fe0078d75f4eb32ab3e1bc06a95e8463b62df
Guilherme Baptista <guisgb13@gmail.com>
2014-02-24 22:25:02 +0100

    Fixing port to 5000 for local testing

--- server.py
+++ server.py
@@ -12,5 +12,5 @@ def index():
 
 if __name__ == "__main__":
 
-	port = int(os.environ.get("PORT", 80))
+	port = int(os.environ.get("PORT", 5000))
 	server.run(host="0.0.0.0", port=port, debug=True)	
